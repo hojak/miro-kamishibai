811b4a0d4d98ec0c9af9fec315f4fa3ade89f918
Bernhard Schenk <18262476+Knesch@users.noreply.github.com>
2022-04-12 17:07:16 +0200

    mob next [ci-skip] [ci skip] [skip ci]

--- src/js/create-widget.js
+++ src/js/create-widget.js
@@ -1,6 +1,7 @@
 const sdk = require('api')('@miro/v1.0#f35y82kvavd4iy');
+const config = require("../../test/http-client.private.env.json");
 
-sdk.auth('');
-sdk['create-board-widgets']({type: 'card', title: 'Simple card 42'}, {id: ''})
+sdk.auth(config.dev.api_token);
+sdk['create-board-widgets']({type: 'card', title: 'Simple card 42'}, {id: config.dev.board_id})
     .then(res => console.log(res))
     .catch(err => console.error(err));
---
+++ src/js/test.js
@@ -0,0 +1,2 @@
+const config = require("../../test/http-client.private.env.json");
+console.log(config);
